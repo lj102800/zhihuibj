9e8679f78897c4c99af65a5c55539a4f0ecc6602
lj102800 <lj102800>
2016-06-30 16:28:31 +0800

    commont

---
+++ zhsh/src/com/example/zhsh/global/Constants.java
@@ -0,0 +1,7 @@
+package com.example.zhsh.global;
+
+public class Constants {
+//	public static final String SERVER_URL="http://192.168.1.70:8080/zhbj";
+	public static final String SERVER_URL="http://192.168.0.106:8080/zhbj";
+	public static final String CATEGORIES_URL=SERVER_URL+"/categories.json";
+}
