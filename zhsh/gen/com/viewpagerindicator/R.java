023b960c7cfc8108a0194a2cf788761d410f6dbe
lj102800 <768082164@qq.com>
2016-08-06 16:54:09 +0800

    ShareSDK

--- zhsh/gen/com/viewpagerindicator/R.java
+++ zhsh/gen/com/viewpagerindicator/R.java
@@ -99,14 +99,14 @@ public final class R {
 		public static final int LinePageIndicator_centered = 1;
 	}
 	public static final class drawable {
-		public static final int vpi__tab_selected_pressed_holo = 0x7f020033;
+		public static final int vpi__tab_selected_pressed_holo = 0x7f020062;
 		public static final int news_tab_item_bg_select = 0x7f020020;
-		public static final int vpi__tab_selected_focused_holo = 0x7f020031;
-		public static final int vpi__tab_selected_holo = 0x7f020032;
-		public static final int vpi__tab_unselected_focused_holo = 0x7f020034;
-		public static final int vpi__tab_unselected_holo = 0x7f020035;
-		public static final int vpi__tab_indicator = 0x7f020030;
-		public static final int vpi__tab_unselected_pressed_holo = 0x7f020036;
+		public static final int vpi__tab_selected_focused_holo = 0x7f020060;
+		public static final int vpi__tab_selected_holo = 0x7f020061;
+		public static final int vpi__tab_unselected_focused_holo = 0x7f020063;
+		public static final int vpi__tab_unselected_holo = 0x7f020064;
+		public static final int vpi__tab_indicator = 0x7f02005f;
+		public static final int vpi__tab_unselected_pressed_holo = 0x7f020065;
 	}
 	public static final class attr {
 		public static final int footerIndicatorStyle = 0x7f010014;
--- zhsh/src/com/example/zhsh/NewsDetailActivity.java
+++ zhsh/src/com/example/zhsh/NewsDetailActivity.java
@@ -11,12 +11,14 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
+import android.webkit.WebSettings.TextSize;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebSettings.TextSize;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -160,7 +162,7 @@ public class NewsDetailActivity extends Activity implements OnClickListener{
 			showChooseDialog();
 			break;
 		case R.id.btn_share:
-
+			showShare();
 			break; 
 
 		default:
@@ -168,4 +170,32 @@ public class NewsDetailActivity extends Activity implements OnClickListener{
 		}
 	
 	}
+	private void showShare() {
+		 ShareSDK.initSDK(this);
+		 OnekeyShare oks = new OnekeyShare();
+		 //关闭sso授权
+		 oks.disableSSOWhenAuthorize(); 
+		 
+		// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+		 //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+		 // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+		 oks.setTitle(getString(R.string.share));
+		 // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+		 oks.setTitleUrl("http://sharesdk.cn");
+		 // text是分享文本，所有平台都需要这个字段
+		 oks.setText("我是分享文本");
+		 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+		 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+		 // url仅在微信（包括好友和朋友圈）中使用
+		 oks.setUrl("http://sharesdk.cn");
+		 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+		 oks.setComment("我是测试评论文本");
+		 // site是分享此内容的网站名称，仅在QQ空间使用
+		 oks.setSite(getString(R.string.app_name));
+		 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+		 oks.setSiteUrl("http://sharesdk.cn");
+		 
+		// 启动分享GUI
+		 oks.show(this);
+		 }
 }
